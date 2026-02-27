5135dc0fc40548b49a5bcfb6130b1b794377d568
Olalekan <efunkunleolalekan@gmail.com>
2026-02-27 18:31:56 +0100

    chore: configure eslint and prettier for code style enforcement
    Adds @typescript-eslint parser and plugin with recommended rules.
    Prettier configured for single quotes, 100-char print width, and
    trailing commas. Unused vars (except _prefixed) are flagged as errors.

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'no-console': 'warn',
+  },
+  env: {
+    node: true,
+    es2021: true,
+  },
+  ignorePatterns: ['dist/', 'node_modules/'],
+};
